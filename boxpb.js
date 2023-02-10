5801a10ef715504cb13b6512b979c5033c1fbbf6
Fernanda Carvalho <100417321+fernandacarvdev@users.noreply.github.com>
2023-02-10 20:12:29 -0300

    Add files via upload

---
+++ boxpb.js
@@ -0,0 +1,11 @@
+const testimonialContainer = document.querySelector('.testimonial-container')
+const testimonial = document.querySelector('.testimonial')
+const userImage = document.querySelector('.user-image')
+const username = document.querySelector('.username')
+const role = document.querySelector('.role')
+
+const testimonials =[{
+    name: 'Megan Ricci',
+    position: 'model',
+    photo: 'ima'
+}]
